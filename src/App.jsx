547376826de0ab7c938b6344faff61b8761c6af2
ASHfaque-sys <mohammadali022005@gmail.com>
2026-03-19 23:36:02 +0530

    fix: remove global chat bubble, only show on results page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import PersonalLoan from './pages/PersonalLoan'
 import BusinessLoan from './pages/BusinessLoan'
@@ -7,15 +7,6 @@ import Results from './pages/Results'
 import Feedback from './pages/Feedback'
 import Auth from './pages/Auth'
 import Dashboard from './pages/Dashboard'
-import FloatingChat from './components/FloatingChat'
-
-// Only show global chat on pages that don't have their own context-aware chat
-function GlobalChat() {
-    const { pathname } = useLocation()
-    // Results page has its own FloatingChat with full report context
-    if (pathname === '/results') return null
-    return <FloatingChat context={null} />
-}
 
 export default function App() {
     return (
@@ -29,7 +20,6 @@ export default function App() {
                 <Route path="/auth" element={<Auth />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
-            <GlobalChat />
         </BrowserRouter>
     )
 }
